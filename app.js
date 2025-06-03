06bd30f617d082e9a3522fa912e960503617f60b
Baptiste Vasseur <baptiste.vasseur@calmedica.fr>
2025-06-03 16:40:21 +0200

    :sparkles: ajout correction exo 1 & 2

---
+++ app.js
@@ -0,0 +1,53 @@
+
+// $maVariable = "Bonjour";
+// $maVariable2 = 42;
+// $maVariable3 = true;
+
+let maVariable = "Bonjour";
+var maVariable2 = "42";
+const maVariable3 = true;
+-
+console.log(maVariable);
+console.log(maVariable2);
+console.log(maVariable3);
+
+const nom = "Baptiste";
+let age = 18;
+const estEtudiant = true;
+
+console.log(nom);
+age = 27;
+console.log(age);
+
+
+
+const chaineDeCaractere1 = "Bonjour";
+const chaineDeCaractere2 = 'Bonjour';
+const chaineDeCaractere3 = `Bonjour`;
+console.log(chaineDeCaractere1, chaineDeCaractere2, chaineDeCaractere3);
+
+// let maVariable = "Baptiste"
+// echo `Bonjour ${maVariable}`;
+
+
+
+
+
+const ville = "Paris";
+const pays = "France";
+
+//const phrase = "J'habite à " + ville + " qui est en " + pays;
+// ou 
+
+
+
+
+
+const phrase = `J'habite à ${ville} qui est en ${pays}`;
+
+// En php : $maPhraseEnMajuscule = strtoupper($phrase)
+// toUpperCase(phrase);
+phrase.
+
+
+console.log(phrase);

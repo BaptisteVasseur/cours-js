9198acc55f68d63af683c2d81c6c4dd88f2b2c0b
Baptiste Vasseur <baptiste.vasseur@calmedica.fr>
2025-09-15 19:20:51 +0200

    ajout correction Séance 5 - Exo 1 - 1&3

---
+++ correction/Cours 5/Exercice1.js
@@ -0,0 +1,58 @@
+async function chargerPosts() {
+    const url = 'https://jsonplaceholder.typicode.com/posts';
+    const options = {method: 'GET'};
+    const divIdContent = document.querySelector('#content')
+    const loadingElement = document.querySelector('#loading');
+
+    loadingElement.style.display = 'block';
+
+    try {
+        const response = await fetch(url, options);
+        const data = await response.json();
+
+        loadingElement.style.display = 'none';
+
+        const newData = data.slice(0, 5);
+        newData.forEach(function(post) {
+            monNouvelElement = document.createElement('div');
+            monNouvelElement.classList.add('post');
+            monNouvelElement.innerHTML = `
+                <h2>${post.title}</h2>
+                <p>${post.body}</p>
+            `;
+
+            divIdContent.appendChild(monNouvelElement);
+        })
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function chargerUtilisateurs() {
+    const url = 'https://jsonplaceholder.typicode.com/users';
+    const options = {method: 'GET'};
+    const divIdContent = document.querySelector('#content')
+    const loadingElement = document.querySelector('#loading');
+
+    loadingElement.style.display = 'block';
+
+    try {
+        const response = await fetch(url, options);
+        const data = await response.json();
+
+        loadingElement.style.display = 'none';
+
+        data.forEach(function(toto) {
+            monNouvelElement = document.createElement('div');
+            monNouvelElement.classList.add('user');
+            monNouvelElement.innerHTML = `
+                <h2>${toto.name}</h2>
+                <p>${toto.company.name}</p>
+            `;
+
+            divIdContent.appendChild(monNouvelElement);
+        })
+    } catch (error) {
+        console.error(error);
+    }
+}

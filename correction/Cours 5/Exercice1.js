70b66c6c5e25b59bfdbeed01695fac3396f49d93
Baptiste Vasseur <baptiste.vasseur@calmedica.fr>
2025-09-15 20:12:42 +0200

    ajout correction Séance 5 - Exo 1 - 2

--- correction/Cours 5/Exercice1.js
+++ correction/Cours 5/Exercice1.js
@@ -56,3 +56,34 @@ async function chargerUtilisateurs() {
         console.error(error);
     }
 }
+
+async function chargerPost() {
+    const idChoisiParLUtilisateur = prompt('Veuillez choisir un id entre 0 et 100 ?');
+
+    if (idChoisiParLUtilisateur === '') {
+        alert('Veuillez choisir un chiffre entre 1 et 100');
+        return;
+    }
+
+    const url = 'https://jsonplaceholder.typicode.com/posts/' + idChoisiParLUtilisateur;
+    const options = {method: 'GET'};
+
+    try {
+        const response = await fetch(url, options);
+        const data = await response.json();
+
+        const element = document.createElement('div');
+        element.classList.add('post');
+        element.innerHTML = `
+            <h2>${data.title}</h2>
+            <p>${data.body}</p>
+        `
+        const contentElement = document.querySelector('#content');
+        contentElement.innerHTML = '';
+        contentElement.appendChild(element);
+
+    } catch (error) {
+        console.error(error);
+    }
+
+}
